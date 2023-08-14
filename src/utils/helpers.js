b30bcfb6ec23e5796d08d4a43a1a8d016a0fd4d0
eric9530873 <eric9530873@gmail.com>
2023-08-15 00:32:32 +0800

    8/15

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import Swal from 'sweetalert2'
 
-const baseURL = 'http://localhost:3000'
+const baseURL = 'https://forum-master-16.onrender.com'
 
 const axiosInstance = axios.create({
     baseURL
